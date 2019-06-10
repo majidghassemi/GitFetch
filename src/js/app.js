a704e49b4c01da5bd1865c1f3f1e997fa0478f12
Thewn <39520326+Thewm@users.noreply.github.com>
2019-06-10 09:10:28 -0700

    improve UX

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,14 @@ const inputValue = document.querySelector(".container__input");
 const submitBtn = document.querySelector(".container__btn");
 const resultBox = document.querySelector(".container__result");
 const resultBox2 = document.querySelector(".container__result2");
+const popUp = document.querySelector(".container__dialog");
+const closePopUp = document.querySelector(".fa-times");
+const popUpHeader = document.querySelector(".container-dialog-h3");
+const popUpBody = document.querySelector(".container-content__main");
+const popUpDescription = document.querySelector(".container-content__sub");
+const usernameDes = `The user name entered has not been found in the Github. This means that the login name is wrong, or the user name has not yet been registered there. Try again.`;
+const errorDes = `This error occurs due to failure to fully capture the information from the server or due to the completion of the number of requests. Try again later`;
+
 const res1 = document.querySelector(".container__res1");
 const res2 = document.querySelector(".container__res2");
 const res3 = document.querySelector(".container__res3");
@@ -26,54 +34,61 @@ const fetchUser = async user => {
 const showData = _ => {
   fetchUser(inputValue.value)
     .then(res => {
-        if (inputValue.value !== "") {
+      if (inputValue.value !== "") {
+        if (res.data.message !== "Not Found") {
+          res1.innerHTML = `Name: <span class="answer">${res.data.name}</span>`;
+          res2.innerHTML = `Bio: <span class="answer">"${res.data.bio}"</span>`;
+          res3.innerHTML = `Repos: <span class="answer">${
+            res.data.public_repos
+          }</span>`;
+          res4.innerHTML = `Followers: <span class="answer">${
+            res.data.followers
+          }</span>`;
+          res5.innerHTML = `Following: <span class="answer">${
+            res.data.following
+          }</span>`;
+          res6.innerHTML = `Company: <span class="answer">${
+            res.data.company
+          }</span>`;
+          res7.innerHTML = `Location: <span class="answer">${
+            res.data.location
+          }</span>`;
+          res8.innerHTML = `URL: <span class="answer"><a href="${
+            res.data.html_url
+          }"target="_blank">${res.data.html_url}</a></span>`;
 
-          if (res.data.message !== "Not Found") {
-            res1.innerHTML = `Name: <span class="answer">${
-              res.data.name
-            }</span>`;
-            res2.innerHTML = `Bio: <span class="answer">"${
-              res.data.bio
-            }"</span>`;
-            res3.innerHTML = `Repos: <span class="answer">${
-              res.data.public_repos
-            }</span>`;
-            res4.innerHTML = `Followers: <span class="answer">${
-              res.data.followers
-            }</span>`;
-            res5.innerHTML = `Following: <span class="answer">${
-              res.data.following
-            }</span>`;
-            res6.innerHTML = `Company: <span class="answer">${
-              res.data.company
-            }</span>`;
-            res7.innerHTML = `Location: <span class="answer">${
-              res.data.location
-            }</span>`;
-            res8.innerHTML = `URL: <span class="answer"><a href="${
-              res.data.html_url
-            }"target="_blank">${res.data.html_url}</a></span>`;
-
-            resultBox.style.backgroundColor = "whitesmoke";
-            resultBox2.style.backgroundColor = "whitesmoke";
-
-            
-          } else {
-              alert(`This username: "${inputValue.value}" does'nt exist`);
-          }
-          }
-
+          resultBox.style.backgroundColor = "whitesmoke";
+          resultBox2.style.backgroundColor = "whitesmoke";
+        } else {
+          popUpGenerator(
+            "Username Not Found",
+            `This username: "${inputValue.value}" not found`,
+            usernameDes
+          );
+        }
+      }
     })
 
     .catch(error => {
-      alert(`Promise are rejected : ${error}`);
+      popUpGenerator(
+        "Error in catch promise",
+        error,
+        errorDes
+      );
     });
-
-    
 };
 
 submitBtn.addEventListener("click", showData);
 
 document.addEventListener("keypress", event => {
-  if (event.keyCode === 13 || event.which === 13) showData();
+  if (event.keyCode === 13 || event.which === 13) {
+    showData();
+  }
+});
+
+
+
+
+closePopUp.addEventListener("click", _ => {
+  popUp.style.display = "none";
 });
